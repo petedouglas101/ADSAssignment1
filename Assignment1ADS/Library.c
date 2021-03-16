a23bc7aecab348b5461669d6a18c3ec802fc5fab
petedouglas101 <c18762431@mytudublin.ie>
2021-03-16 15:11:52 +0000

    First commit: Added menu

---
+++ Assignment1ADS/Library.c
@@ -0,0 +1,10 @@
+#define _CRT_SECURE_NO_WARNINGS //Allows me to use scanf instead of scanf_f
+#include <stdio.h>//Standard input/output
+
+void main() {
+	int option;//Variable to hold option for menu
+	printf("Welcome to the Library!\nPlease select from one of the following options: \n");//Welcome message of menu
+	printf("1. Add a new book\n2. Allow customer to take out book\n3. Allow customer to return a book\n4. Delete an old book from stock\n5. View all books\n6. View a specific book\n7. View details of most popular and least popular books\n8. Exit");//Displaying the menu and the options
+
+
+}
